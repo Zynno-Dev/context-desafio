9e187d03b3beed9da710456695437ba589f1bdfb
Brian Michelin <michelinbriandev@gmail.com>
2021-04-27 00:05:00 -0300

    4

--- src/Components/MobileContainer/MobileContainer.js
+++ src/Components/MobileContainer/MobileContainer.js
@@ -1,32 +1,15 @@
 import React, { useContext, useState } from 'react';
 import { MobieContext } from '../../MobileContext/MobileContext';
-import Mobilee from '../Mobile/Mobile';
+import Mobile from '../Mobile/Mobile';
 
-const MovieList = () => {
-  const value = useContext(MovieContext);
-  // const [movies, setMovies] = useState([
-  //   {
-  //     name: 'Saving Private Ryan',
-  //     price: '$10',
-  //     id: 1234,
-  //   },
-  //   {
-  //     name: 'Fight Club',
-  //     price: '$12',
-  //     id: 9090,
-  //   },
-  //   {
-  //     name: 'Terminator 1',
-  //     price: '$8',
-  //     id: 1763,
-  //   },
-  // ]);
+const MobileConainer = () => {
+  const value = useContext(MobileContext);
 
   return (
     <div>
-      {/* {movies.map((movie) => {
-        return <Movie name={movie.name} />;
-      })} */}
+       {value.map((value) => {
+        return <Mobie name={mobile.name} />;
+      })} 
       <h1>{value}</h1>
     </div>
   );
